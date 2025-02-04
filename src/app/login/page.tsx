60d5ce09206af592b4769f4c32a9c75ea8e4e966
Lucas sobczak <lucassobczak3@gmail.com>
2025-02-03 22:54:24 -0500

    setup

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Login() {
+    return (
+        <div>
+            <p>login here</p>
+            <Button>Click me</Button>
+        </div>
+    )
+}
---
+++ src/components/navigation/nav-bar.tsx
@@ -0,0 +1,8 @@
+export default function NavigationBar() {
+    return (
+        <div>
+            <p className="text-white">Rora</p>
+        </div>
+
+    )
+}
